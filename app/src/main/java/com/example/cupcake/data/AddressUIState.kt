343ed2684421d70fcf30c1a5e6263c8de2158016
William Shen <william.shen.0001@gmail.com>
2024-06-18 12:35:33 -0700

    initial commit

---
+++ app/src/main/java/com/example/cupcake/data/AddressUIState.kt
@@ -0,0 +1,17 @@
+package com.example.cupcake.data
+
+
+data class AddressUIState(
+    val address1: String = "",
+    val address2: String = "",
+    val city: String ="",
+    val state: String = "",
+    val country: String = "",
+    val zipcode: String = "",
+    val name: String = "",
+    val cardNum: String = "",
+    val expiration: String = "",
+    val secCode: String = "",
+
+)
+
